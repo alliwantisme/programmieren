151347094bd9be78f457990071e0af4932437bd2
alliwantisme <96227952+alliwantisme@users.noreply.github.com>
2021-12-16 09:03:02 +0100

    Add files via upload

---
+++ 1-1000.py
@@ -0,0 +1,5 @@
+i = 1             #Definition von i 
+while i <= 1000:  #wenn i kleiner gleich 1000 ist:
+  print(i)        #anzeige von i
+  i = i + 1       #nach der Ausgabe von i, wird i um 1 addiert
+                  #Es ist ein While-Loop, darum wird das Programm wiederholt bis es 1000 erreicht hat.
